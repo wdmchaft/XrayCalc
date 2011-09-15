579af8bb90ee4cb6554d3e02a86b94d7e7cd7d63
Crystalmyst <tim@robbmob.com>
2011-09-15 23:35:08 +1200

    Initial Commit

---
+++ XrayCalc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XrayCalc
+//
+//  Created by  on 20110915.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
